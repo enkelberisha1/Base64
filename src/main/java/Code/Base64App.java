1ab5b18762a9cb6b9c0d4d4fd8b1ebd9ff2caeb2
Enesiktm <enesispahiu@gmail.com>
2025-04-20 15:42:38 +0200

    Base64app

--- src/main/java/Code/Base64App.java
+++ src/main/java/Code/Base64App.java
@@ -4,6 +4,16 @@ package Code;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Interface.fxml"));
-        stage.setTitle("Base64 Encoder/Decoder");
-        stage.setScene(new Scene(fxmlLoader.load()));
+
+import javafx.stage.Stage;
+public class Base64App extends Application {
+        @Override
+        public void start(Stage stage) throws Exception {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Interface.fxml"));
+                stage.setTitle("Base64 Encoder/Decoder");
+                stage.setScene(new Scene(fxmlLoader.load()));
+
+
+        }
+
+}
